a4c1ae16cad70d01fbc0707126d53a3e0ec4c16b
Denis <makhonkodenis@gmail.com>
2019-04-08 10:20:08 +0300

    UPD: pingback

--- header.php
+++ header.php
@@ -14,7 +14,6 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <?php wp_head(); ?>
 </head>
